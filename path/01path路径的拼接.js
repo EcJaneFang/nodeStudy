1ac3844fc5f8673177fc3c13cb7790720dfe0063
詹方 <804571770@qq.com>
2022-10-24 22:36:53 +0800

    path模块 join方法的使用

---
+++ path/01path路径的拼接.js
@@ -0,0 +1,9 @@
+const path = require("path")
+
+const fl1 = "data1"
+const fl2 = "data1"
+const fl3 = "data1"
+const fl4 = "data1"
+const fileName = "1.text"
+const fl5 = path.join(fl1, fl2, fl3, fl4, fileName)
+console.log(fl5);
